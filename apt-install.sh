0f882249a469c67309e1166ee1bc40a994809bc8
Emil Marklund <eeemil@acc.umu.se>
2018-09-03 16:28:26 +0200

    Added apt installment mechanism

---
+++ apt-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update
+# Base utilities
+sudo apt install emacs terminator chromium-browser chrome-gnome-shell thunderbird
+# Dev packages
+sudo apt install git build-essential zsh
+# Python
+sudo apt install python3 python3-pip virtualenv
