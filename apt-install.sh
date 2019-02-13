abdf458463992407c026d8b683e5e899d56b5887
Emil Marklund <emil.marklund@vkmedia.se>
2019-02-13 13:37:26 +0100

    Pygments is now installed through pip
    Older pygments versions had a weak lexer for yaml

--- apt-install.sh
+++ apt-install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 sudo apt update
 # Base utilities
-sudo apt install -y emacs terminator chromium-browser chrome-gnome-shell thunderbird curl gnome-tweak-tool
+udo apt install -y emacs terminator chromium-browser chrome-gnome-shell thunderbird curl gnome-tweak-tool
 # Dev packages
-sudo apt install -y git build-essential zsh python-pygments
+sudo apt install -y git build-essential zsh
 # Python
 sudo apt install -y python3 python3-pip virtualenv
+
+sudo -H pip3 install --upgrade pip
+pip3 install Pygments
