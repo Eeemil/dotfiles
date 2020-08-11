1a05bd1d707c718ee9e1783deb699bb14f555b84
Emil Marklund <emil.marklund@vkmedia.se>
2020-08-11 15:02:32 +0200

    Add ripgrep

--- apt-install.sh
+++ apt-install.sh
@@ -2,6 +2,8 @@
 sudo apt update
 # Base utilities
 sudo apt install -y emacs terminator chromium-browser chrome-gnome-shell thunderbird curl gnome-tweak-tool moreutils
+# Ripgrep: very fast git-aware grep
+sudo apt install -y ripgrep
 # Dev packages
 sudo apt install -y git build-essential zsh
 # Python
