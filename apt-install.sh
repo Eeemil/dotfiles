57f26add5cbac76e3609e873b7e13d8694f5e810
Emil Marklund <emil.marklund@vkmedia.se>
2020-12-16 13:51:11 +0100

    Add more default packages

--- apt-install.sh
+++ apt-install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sudo apt update
 # Base utilities
-sudo apt install -y emacs terminator chromium-browser chrome-gnome-shell thunderbird curl gnome-tweak-tool moreutils
+sudo apt install -y emacs terminator chromium-browser chrome-gnome-shell thunderbird curl gnome-tweak-tool moreutils xclip
 # Ripgrep: very fast git-aware grep
 sudo apt install -y ripgrep
 # Dev packages
-sudo apt install -y git build-essential zsh
+sudo apt install -y git build-essential zsh jq
 # Python
 sudo apt install -y python3 python3-pip virtualenv
 
