d4c91de9520d71fc12bd3f717a705a754808f3c3
Emil Marklund <emil.marklund@vkmedia.se>
2021-02-18 11:08:52 +0100

    FINALLY remove built-in gnome keybindings for super-[1-9]

--- gnome-settings/load-settings.sh
+++ gnome-settings/load-settings.sh
@@ -44,6 +44,19 @@ dconf load /org/gnome/desktop/wm/keybindings/ <<EOF
 toggle-maximized=['']
 EOF
 
+dconf load /org/gnome/shell/keybindings/ <<EOF
+[/]
+switch-to-application-1=@as []
+switch-to-application-2=@as []
+switch-to-application-3=@as []
+switch-to-application-4=@as []
+switch-to-application-5=@as []
+switch-to-application-6=@as []
+switch-to-application-7=@as []
+switch-to-application-8=@as []
+switch-to-application-9=@as []
+EOF
+
 # Finally found a method for disabling the Super-<num> keybinding: https://askubuntu.com/questions/968103/disable-the-default-app-key-supernum-functionality-on-ubuntu-17-10-and-later
 
 gsettings set org.gnome.shell.extensions.dash-to-dock hot-keys false
